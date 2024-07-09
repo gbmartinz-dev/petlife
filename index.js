94f6ef11a0dd95eabf70abc253a5fffc62ac73f8
gabriellsux <gabrielpereiramartins49@gmail.com>
2024-07-09 18:03:32 -0300

    Inserting js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-document.querySelectorAll("#inicio")
+var elementosDuvida = document.querySelectorAll('.duvida')
+
+elementosDuvida.forEach(function (duvida) {
+    duvida.addEventListener('click', function() {
+        duvida.classList.add('fundo-verde')
+    })
+})
